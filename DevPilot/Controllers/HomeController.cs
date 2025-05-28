75b55c29f5764bcff4387b8898ad396343e3f548
Gouravva Tadahal <gtadahal@navex.com>
2025-05-28 11:53:20 +0530

    added LD

--- DevPilot/Controllers/HomeController.cs
+++ DevPilot/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.FeatureManagement;
+﻿using LaunchDarkly.Client;
+using Microsoft.FeatureManagement;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -11,9 +13,6 @@ namespace DevPilot.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
-        {
-        }
         public ActionResult Index()
         {
             return View();
@@ -40,16 +39,24 @@ namespace DevPilot.Controllers
         }
         public async Task<ActionResult> Features()
         {
+            bool newFeatureEnabled = new LaunchDarkly.Client.LdClient("sdk-2d6d3a0c-2a0d-4d3c-8b6a-2a0d4d3c8b6a").BoolVariation("LDNewFeature", new User("user-key"));
             var featureManager = (FeatureManager)HttpContext.Application["FeatureManager"];
             try
             {
-                if (await featureManager.IsEnabledAsync("NewFeatureTest"))
+                if ( !newFeatureEnabled && await featureManager.IsEnabledAsync("NewFeature") )
                 {
-                    return Redirect("~/UserList.aspx");
+                    if (await featureManager.IsEnabledAsync("BetaFeature"))
+                    {
+                        return Redirect("~/UserList.aspx");
+                    }
+                    else
+                    {
+                        return Redirect("~/Error.asp");
+                    }
                 }
                 else
                 {
-                    return Redirect("~/Error.asp");
+                    return RedirectToAction("Users", "Home");
                 }
             }
             catch (Exception ex)
