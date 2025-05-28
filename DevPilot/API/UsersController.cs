0e6e7c04bbfbf58a139c20fb04afafc1245e1441
Gouravva Tadahal <gtadahal@navex.com>
2025-05-28 14:22:22 +0530

    adding feature toggle

--- DevPilot/API/UsersController.cs
+++ DevPilot/API/UsersController.cs
@@ -1,21 +1,32 @@
 ﻿using DevPilot.Models;
+using LaunchDarkly.Client;
+using Microsoft.FeatureManagement;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 
 namespace DevPilot.API
 {
     public class UsersController : ApiController
     {
-        public IHttpActionResult Get()
+        public async Task<IHttpActionResult> Get()
         {
-            var users = new List<Users>
+            var featureManager = (FeatureManager)HttpContext.Current.Application["FeatureManager"];
+            if (await featureManager.IsEnabledAsync("NewFeature"))
             {
-                new Users { Id = 1, Name = "Alice", AccessLevel = "Admin" },
-                new Users { Id = 2, Name = "Bob", AccessLevel = "Editor" },
-                new Users { Id = 3, Name = "Charlie", AccessLevel = "Viewer" }
-            };
-
-            return Ok(users);
+                var users = new List<Users>
+                        {
+                            new Users { Id = 1, Name = "Alice", AccessLevel = "Admin" },
+                            new Users { Id = 2, Name = "Bob", AccessLevel = "Editor" },
+                            new Users { Id = 3, Name = "Charlie", AccessLevel = "Viewer" }
+                        };
+                return Ok(users);
+            }
+            else
+            {
+                return Ok(new List<Users>());
+            }
         }
     }
 }
